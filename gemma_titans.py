6dba29b384ad4564be148bf7f84965ba8e9a92cb
andrew-veriga <andrew.veriga@gmail.com>
2026-03-24 22:34:09 +0400

    chunk 32

--- gemma_titans.py
+++ gemma_titans.py
@@ -55,7 +55,7 @@ class TitansBlock(_modules.Block):
             dim=self.embed_dim,
             heads=self.num_heads,
             dim_head=256, # фиксированная размерность головы для памяти
-            chunk_size=16,
+            chunk_size=32,
         )
         
         # 1152 независимых вентиля
