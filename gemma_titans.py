507d0a876cd7229e588b8272d6cf488e82b8aed8
andrew-veriga <andrew.veriga@gmail.com>
2026-03-25 23:36:43 +0400

    fix syntax

--- gemma_titans.py
+++ gemma_titans.py
@@ -55,7 +55,7 @@ class TitansBlock(_modules.Block):
             dim=self.embed_dim,
             heads=self.num_heads,
             dim_head=256, # фиксированная размерность головы для памяти
-            chunk_size=32 # размерность чанка для обработки памяти (можно настроить)
+            chunk_size=32, # размерность чанка для обработки памяти (можно настроить)
             max_grad_norm=1.0 # для стабильности обучения
         )
         
